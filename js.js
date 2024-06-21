9543a834208c3bcb5b2395394be5e77fd682d9f1
AdamRomero17 <jhonadamabecillaromero@gmail.com>
2024-06-21 12:15:22 +0800

    optimize nav bar for making it sticky position.

---
+++ js.js
@@ -0,0 +1,16 @@
+
+
+// function a() {
+//     let a = 3 + 5;
+//     console.log(a)
+// }
+// a();
+// console.log("end")
+
+console.log("start")
+
+setTimeout( function cb(){
+    console.log("Callback")
+},3000 )
+
+console.log("End")
