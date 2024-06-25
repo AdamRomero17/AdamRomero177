bc34fb76dc6dbc840a853084df445f84242b075f
AdamRomero17 <jhonadamabecillaromero@gmail.com>
2024-06-25 16:19:54 +0800

    Changing style and adding javascript for nav for changing its color when the screen being scrolled. And minor edit to index page.

--- javascript/asidenav.js
+++ javascript/asidenav.js
@@ -1,3 +1,4 @@
+/* Buttona for navigation */
 const toggleBtn = document.querySelector(".sidebar-toggle");
 const closeBtn = document.querySelector(".close-Button");
 const sidebar = document.querySelector(".sidebar");
@@ -16,3 +17,30 @@ sidebar.classList.toggle('show-sidebar')
 closeBtn.addEventListener("click", function(){
     sidebar.classList.remove('show-sidebar')
 })
+
+/* Changing color of nav when screen being scroll */
+window.addEventListener('scroll', function() {
+    const colorChangeElement = document.querySelector('.nav');
+    const logoN = document.querySelector('.logoname')
+    const scrolling = window.scrollY;
+    if(scrolling > 0){
+        colorChangeElement.classList.remove('scroll');
+        logoN.classList.remove('logonameWhite');
+    }else{
+        colorChangeElement.classList.add('scroll');
+        logoN.classList.add('logonameWhite');
+    }
+    // colorChangeElement.style.backgroundColor = 'red'; // Change color to red when scrolling
+});
+window.addEventListener('scroll', function() {
+    const whiteF = document.querySelectorAll('.whiteFont')
+    const scrolling = window.scrollY;
+        whiteF.forEach(classElements => {
+            if(scrolling > 0){  
+                classElements.style.color = 'white';
+            }else{
+                classElements.style.color = 'black';
+            }
+        });
+    // colorChangeElement.style.backgroundColor = 'red'; // Change color to red when scrolling
+});
